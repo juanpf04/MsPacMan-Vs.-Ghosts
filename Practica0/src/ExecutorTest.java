834afd8c20facc97869a466e066ea2af26fa3c86
juanpf04 <juanpf04@ucm.es>
2024-09-18 18:12:08 +0200

    Upload

---
+++ Practica0/src/ExecutorTest.java
@@ -0,0 +1,24 @@
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsRandom;
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRandom;
+import pacman.Executor;
+import pacman.controllers.GhostController;
+import pacman.controllers.PacmanController;
+
+public class ExecutorTest {
+
+    public static void main(String[] args) {
+        Executor executor = new Executor.Builder()
+                .setTickLimit(4000)
+                .setVisual(true)
+                .setScaleFactor(2.5)
+                .build();
+
+        PacmanController pacMan = new MsPacManRandom();
+        GhostController ghosts = new GhostsRandom();
+        
+        System.out.println( 
+            executor.runGame(pacMan, ghosts, 30) //last parameter defines speed
+        );     
+    }
+	
+}
---
+++ Practica2/src/es/ucm/fdi/ici/c2425/practica2/grupoYY/mspacman/MsPacManInput.java
@@ -0,0 +1,19 @@
+package es.ucm.fdi.ici.c2425.practica2.grupoYY.mspacman;
+
+import es.ucm.fdi.ici.Input;
+import pacman.game.Game;
+
+public class MsPacManInput extends Input {
+
+	public MsPacManInput(Game game) {
+		super(game);
+		
+	}
+
+	@Override
+	public void parseInput() {
+		// does nothing.
+
+	}
+
+}
