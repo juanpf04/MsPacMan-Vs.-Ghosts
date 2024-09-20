3c4fbfc8f5aba1324806104d3a260fe823a7b647
juanpf04 <juanpf04@ucm.es>
2024-09-20 12:35:59 +0200

    switch

--- Practica0/src/ExecutorTest.java
+++ Practica0/src/ExecutorTest.java
@@ -1,7 +1,13 @@
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.Ghosts;
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsAggresive;
 import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsRandom;
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacMan;
 import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRandom;
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRunAway;
 import pacman.Executor;
 import pacman.controllers.GhostController;
+import pacman.controllers.HumanController;
+import pacman.controllers.KeyBoardInput;
 import pacman.controllers.PacmanController;
 
 public class ExecutorTest {
@@ -13,11 +19,39 @@ public class ExecutorTest {
                 .setScaleFactor(2.5)
                 .build();
 
-        PacmanController pacMan = new MsPacManRandom();
-        GhostController ghosts = new GhostsRandom();
+        PacmanController pacMan = null; 
+        
+        switch (3) {
+		case 1: 
+			pacMan = new HumanController(new KeyBoardInput());
+			break;
+		case 2: 
+			pacMan = new MsPacManRandom();
+			break;
+		case 3: 
+			pacMan = new MsPacManRunAway();
+			break;			
+		case 4: 
+			pacMan = new MsPacMan();
+			break;			
+		}
+        
+        GhostController ghosts = null;
+        
+        switch (2) {
+		case 1: 
+			ghosts = new GhostsRandom();
+			break;
+		case 2: 
+			ghosts = new GhostsAggresive();
+			break;			
+		case 3: 
+			ghosts = new Ghosts();
+			break;			
+		}
         
         System.out.println( 
-            executor.runGame(pacMan, ghosts, 30) //last parameter defines speed
+            executor.runGame(pacMan, ghosts, 50) //last parameter defines speed
         );     
     }
 	
