7fb14bbf40221ccd3605d037f9bb01442bde5161
juanpf04 <juanpf04@ucm.es>
2024-09-20 12:36:35 +0200

    Aggresive Ghosts

--- Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/GhostsAggresive.java
+++ Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/GhostsAggresive.java
@@ -1,30 +1,35 @@
 package es.ucm.fdi.ici.c2425.practica0.grupoIndividual;
 
 import java.util.EnumMap;
-import java.util.Random;
 
 import pacman.controllers.GhostController;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
 public final class GhostsAggresive extends GhostController {
-    private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
-    private MOVE[] allMoves = MOVE.values();
-    private Random rnd = new Random();
 
-    @Override
-    public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
-        moves.clear();
-        for (GHOST ghostType : GHOST.values()) {
-            if (game.doesGhostRequireAction(ghostType)) {
-                moves.put(ghostType, allMoves[rnd.nextInt(allMoves.length)]);
-            }
-        }
-        return moves;
-    }
-    
-    public String getName() {
-    	return "GhostsRandom";
-    }
+	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
+
+	@Override
+	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		moves.clear();
+
+		int mspacman = game.getPacmanCurrentNodeIndex();
+		
+		for (GHOST ghostType : GHOST.values()) {
+			if (game.doesGhostRequireAction(ghostType)) {
+				int ghost = game.getGhostCurrentNodeIndex(ghostType);
+				moves.put(ghostType, game.getApproximateNextMoveTowardsTarget(ghost, mspacman,
+						game.getGhostLastMoveMade(ghostType), DM.PATH));
+			}
+		}
+
+		return moves;
+	}
+
+	public String getName() {
+		return "GhostsAggresive";
+	}
 }
