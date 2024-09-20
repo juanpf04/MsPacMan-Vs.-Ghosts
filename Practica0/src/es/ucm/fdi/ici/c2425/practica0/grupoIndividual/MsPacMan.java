9a220ed2f01cff4d15e83195023ae386e0ecdda9
juanpf04 <juanpf04@ucm.es>
2024-09-20 15:37:52 +0200

    Update MsPacMan.java

--- Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/MsPacMan.java
+++ Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/MsPacMan.java
@@ -1,41 +1,23 @@
 package es.ucm.fdi.ici.c2425.practica0.grupoIndividual;
 
 import java.awt.Color;
-import java.util.Random;
 
 import pacman.controllers.PacmanController;
 import pacman.game.Constants.MOVE;
 import pacman.game.Constants.GHOST;
 import pacman.game.Game;
-import pacman.game.GameView;
 
 public class MsPacMan extends PacmanController {
 
-	private Random rnd = new Random();
-	private MOVE[] allMoves = MOVE.values();
-
-	private Color[] colours = { Color.RED, Color.PINK, Color.CYAN, Color.ORANGE };
-
+	private final static Color[] COLOURS = { Color.RED, Color.PINK, Color.CYAN, Color.ORANGE };
+	private final static int LIMIT_DISTANCE = 20;
+	
 	@Override
 	public MOVE getMove(Game game, long timeDue) {
 
-		// Show lines to Pills
-//    	int[] activePowerPills = game.getActivePillsIndices();
-//    	
-//    	for(int i = 0; i< activePowerPills.length;i++) {
-//    		GameView.addLines(game, Color.CYAN, game.getPacmanCurrentNodeIndex(), activePowerPills[i]);
-//    	}
+		
 
-		// Show way to ghosts
-		for (GHOST ghostType : GHOST.values()) {
-			int ghost = game.getGhostCurrentNodeIndex(ghostType);
-			int mspacman = game.getPacmanCurrentNodeIndex();
-			if (game.getGhostLairTime(ghostType) <= 0)
-				GameView.addPoints(game, colours[ghostType.ordinal()],
-						game.getShortestPath(ghost, mspacman, game.getGhostLastMoveMade(ghostType)));
-		}
-
-		return allMoves[rnd.nextInt(allMoves.length)];
+		return null;
 	}
 
 	public String getName() {
