7bd59bfc15e74acadab0b9e553f5265f0da976cf
ArionScheid <arionscheid@gmail.com>
2024-09-22 22:03:30 +0200

    task 3 done, lines fixed

--- Practica0/src/ExecutorTest.java
+++ Practica0/src/ExecutorTest.java
@@ -1,13 +1,14 @@
-import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsAggresive;
-import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsRandom;
-import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRandom;
-import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRunAway;
+//import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsAggresive;
+//import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.GhostsRandom;
+//import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRandom;
+//import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacManRunAway;
+//import pacman.controllers.HumanController;
+//import pacman.controllers.KeyBoardInput;
+
 import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.MsPacMan;
-//import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.Ghost;
+import es.ucm.fdi.ici.c2425.practica0.grupoIndividual.Ghosts;
 import pacman.Executor;
 import pacman.controllers.GhostController;
-import pacman.controllers.HumanController;
-import pacman.controllers.KeyBoardInput;
 import pacman.controllers.PacmanController;
 
 public class ExecutorTest {
@@ -22,7 +23,7 @@ public class ExecutorTest {
         PacmanController pacMan = new MsPacMan();
         // PacmanController pacMan = new MsPacManRunAway();
         //PacmanController pacMan = new HumanController(new KeyBoardInput());
-        GhostController ghosts = new GhostsRandom();
+        GhostController ghosts = new Ghosts();
         // GhostController ghosts = new GhostsAggresive();
         System.out.println( 
             executor.runGame(pacMan, ghosts, 30) //last parameter defines speed
--- Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/Ghosts.java
+++ Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/Ghosts.java
@@ -1,5 +1,72 @@
 package es.ucm.fdi.ici.c2425.practica0.grupoIndividual;
 
-public class Ghosts {
+import java.util.EnumMap;
+import java.util.Random;
+
+import pacman.controllers.GhostController;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+
+public class Ghosts extends GhostController {
+    private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
+
+    @Override
+    public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+        moves.clear();
+        for (GHOST ghostType : GHOST.values()) {
+            if (game.doesGhostRequireAction(ghostType)) {
+                int ghostNodeIndex = game.getGhostCurrentNodeIndex(ghostType);
+                int pacmanNodeIndex = game.getPacmanCurrentNodeIndex();
+                MOVE lastGhostMove = game.getGhostLastMoveMade(ghostType);
+
+                if (isEdible(game,ghostType) || isPMCloseToPPill(game, 15)) {
+                    MOVE nextMove = game.getApproximateNextMoveTowardsTarget(
+                        ghostNodeIndex,
+                        pacmanNodeIndex,
+                        lastGhostMove,
+                        DM.PATH).opposite();
+                    moves.put(ghostType,nextMove);
+                } else {
+					if(Math.random() < 0.9) {
+                        MOVE nextMove = game.getNextMoveTowardsTarget(
+                            ghostNodeIndex, 
+                            pacmanNodeIndex,
+                            DM.PATH);
+						moves.put(ghostType, nextMove);
+					}
+					else {
+                        Random rnd = new Random();
+                        MOVE nextMove = MOVE.values()[rnd.nextInt(MOVE.values().length)];
+						moves.put(ghostType, nextMove);
+					}
+				}
+            }
+        }
+        return moves;
+    }
     
+
+    private boolean isEdible(Game game, GHOST ghost) {
+        return game.getGhostEdibleTime(ghost) > 0;
+    }
+
+
+    private boolean isPMCloseToPPill(Game game, double limit) {
+        int pacmanNodeIndex = game.getPacmanCurrentNodeIndex();
+        int[] ppills = game.getActivePowerPillsIndices(); 
+        for (int ppill : ppills) {
+            double dist = game.getDistance(pacmanNodeIndex, ppill, DM.PATH);
+            if (dist < limit) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getName() {
+    	return "Ghosts";
+    }
 }
