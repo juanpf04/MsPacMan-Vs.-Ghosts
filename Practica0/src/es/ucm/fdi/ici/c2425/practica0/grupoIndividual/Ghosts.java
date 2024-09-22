1f86cc409884d3152a2f50eae648ff42709832b9
juanpf04 <juanpf04@ucm.es>
2024-09-22 14:42:11 +0200

    JGhost

--- Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/Ghosts.java
+++ Practica0/src/es/ucm/fdi/ici/c2425/practica0/grupoIndividual/Ghosts.java
@@ -4,27 +4,54 @@ import java.util.EnumMap;
 import java.util.Random;
 
 import pacman.controllers.GhostController;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
 public final class Ghosts extends GhostController {
-    private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
-    private MOVE[] allMoves = MOVE.values();
-    private Random rnd = new Random();
 
-    @Override
-    public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
-        moves.clear();
-        for (GHOST ghostType : GHOST.values()) {
-            if (game.doesGhostRequireAction(ghostType)) {
-                moves.put(ghostType, allMoves[rnd.nextInt(allMoves.length)]);
-            }
-        }
-        return moves;
-    }
-    
-    public String getName() {
-    	return "JGhosts";
-    }
+	private final static int LIMIT_DISTANCE = 50;
+
+	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
+	private MOVE[] allMoves = MOVE.values();
+	private Random rnd = new Random();
+
+	@Override
+	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		this.moves.clear();
+
+		int mspacman = game.getPacmanCurrentNodeIndex();
+
+		for (GHOST ghost : GHOST.values()) {
+			if (game.doesGhostRequireAction(ghost)) {
+
+				if (game.isGhostEdible(ghost) || isPacmanCloseToPowerPill(game))
+					this.moves.put(ghost,
+							game.getNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghost), mspacman, DM.PATH));
+				else {
+					if (this.rnd.nextFloat() < 0.9)
+						this.moves.put(ghost,
+								game.getNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghost), mspacman, DM.PATH));
+					else
+						this.moves.put(ghost, this.allMoves[this.rnd.nextInt(this.allMoves.length)]);
+
+				}
+			}
+		}
+		return this.moves;
+	}
+
+	private boolean isPacmanCloseToPowerPill(Game game) {
+		for (int powerPill : game.getActivePowerPillsIndices()) {
+			if (game.getShortestPathDistance(game.getPacmanCurrentNodeIndex(), powerPill) < LIMIT_DISTANCE)
+				return true;
+		}
+
+		return false;
+	}
+
+	public String getName() {
+		return "JGhosts";
+	}
 }
