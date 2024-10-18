afc08a37cc715a5eb8b3d8aee84205658d571d82
ArionScheid <arionscheid@gmail.com>
2024-10-18 16:17:31 +0200

    some auxiliary functions

--- Practica2/src/es/ucm/fdi/ici/c2425/practica2/grupo02/ghosts/GhostsInfo.java
+++ Practica2/src/es/ucm/fdi/ici/c2425/practica2/grupo02/ghosts/GhostsInfo.java
@@ -7,37 +7,80 @@ import pacman.game.Constants.GHOST;
 
 public class GhostsInfo {
 
+	private int pacmanNextJunction;
+	private int nearestEdibleGhostToPacman;
+
+	// nearest not edible ghost to nearest edible ghost to pacman
+	private int nearestGhost; 
+
+	private Map<GHOST, Double> distanceGhostToPacman;
 	private Map<GHOST, Integer> nearestPPill;
-	private Map<GHOST, Integer> nearestPill;
-	private Map<GHOST, Integer> nearestGhost;
+	private Map<GHOST, Double> distanceToNearestPPill;
+	//private Map<GHOST, Integer> nearestGhost;
 
 	public GhostsInfo() {
 		this.nearestPPill = new HashMap<GHOST, Integer>();
-		this.nearestPill = new HashMap<GHOST, Integer>();
-		this.nearestGhost = new HashMap<GHOST, Integer>();
+		//this.nearestGhost = new HashMap<GHOST, Integer>();
+		this.distanceToNearestPPill = new HashMap<GHOST, Double>();
+	}
+
+
+	// getters
+
+	public int getNearestGhost() {
+		return this.nearestGhost;
+	}
+
+	/**
+	 * Node of nearest edible ghost to pacman. Returns -1 if no ghost is edbile.
+	 * @param node
+	 */
+	public int getNearestEdibleGhostToPacman() {
+		return this.nearestEdibleGhostToPacman;
+	}
+
+	public double getDistanceGhostToPacman(GHOST ghost) {
+		return this.distanceGhostToPacman.get(ghost);
+	}
+
+	public int getPacmanNextJunction() {
+		return pacmanNextJunction;
 	}
 
 	public int getNearestPPill(GHOST ghost) {
 		return this.nearestPPill.get(ghost);
 	}
 
-	public int getNearestPill(GHOST ghost) {
-		return this.nearestPill.get(ghost);
+	public double getDistanceToNearestPPill(GHOST ghost) {
+		return this.distanceToNearestPPill.get(ghost);
 	}
 
-	public int getNearestGhost(GHOST ghost) {
-		return this.nearestGhost.get(ghost);
+
+
+	// setters
+
+	public void setNearestGhost(int node) {
+		this.nearestGhost = node;
 	}
 
-	public void setNearesPPill(GHOST ghost, int index) {
+	public void setNearestEdibleGhostToPacman(int node) {
+		this.nearestEdibleGhostToPacman = node;
+	}
+
+	public void setPacmanNextJunction(int node) {
+		this.pacmanNextJunction = node;
+	}
+
+	public void setNearestPPill(GHOST ghost, int index) {
 		this.nearestPPill.put(ghost, index);
 	}
 
-	public void setNearestGhost(GHOST ghost, int index) {
-		this.nearestGhost.put(ghost, index);
+
+	public void setDistanceToNearestPPill(GHOST ghost, double distance) {
+		this.distanceToNearestPPill.put(ghost,distance);
 	}
 
-	public void setNearesPill(GHOST ghost, int index) {
-		this.nearestPill.put(ghost, index);
+	public void setDistanceGhostToPacman(GHOST ghost, double distance) {
+		this.distanceGhostToPacman.put(ghost,distance);
 	}
 }
--- Practica2/src/es/ucm/fdi/ici/c2425/practica2/grupo02/ghosts/GhostsInput.java
+++ Practica2/src/es/ucm/fdi/ici/c2425/practica2/grupo02/ghosts/GhostsInput.java
@@ -3,6 +3,7 @@ package es.ucm.fdi.ici.c2425.practica2.grupo02.ghosts;
 import es.ucm.fdi.ici.Input;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
 public class GhostsInput extends Input {
@@ -12,6 +13,7 @@ public class GhostsInput extends Input {
 	private boolean PINKYedible;
 	private boolean SUEedible;
 	private double minPacmanDistancePPill;
+	private int closestPPillToPacman;
 	private GhostsInfo info;
 
 	public GhostsInput(Game game, GhostsInfo info) {
@@ -27,11 +29,63 @@ public class GhostsInput extends Input {
 		this.SUEedible = game.isGhostEdible(GHOST.SUE);
 
 		int pacman = game.getPacmanCurrentNodeIndex();
+		this.info.setPacmanNextJunction(getNextJunctionNode(pacman, game.getPacmanLastMoveMade()));
+
 		this.minPacmanDistancePPill = Double.MAX_VALUE;
+
 		for (int ppill : game.getPowerPillIndices()) {
+
+			// find closest ppill to pacman and distance to it
 			double distance = game.getDistance(pacman, ppill, DM.PATH);
-			this.minPacmanDistancePPill = Math.min(distance, this.minPacmanDistancePPill);
+			if (distance < this.minPacmanDistancePPill) {
+				this.minPacmanDistancePPill = distance;
+				this.closestPPillToPacman = ppill;
+			}
+
+			// ghost distances to pacman
+			for (GHOST ghost : GHOST.values()) {
+				double ghostDistance = game.getDistance(game.getGhostCurrentNodeIndex(ghost),ppill,DM.PATH);
+				if (this.info.getDistanceToNearestPPill(ghost) > ghostDistance) {
+					this.info.setDistanceToNearestPPill(ghost, ghostDistance);
+					this.info.setNearestPPill(ghost, ppill);
+				}
+			}
 		}
+
+		// ghost dtstances to pacman, nearest edible ghost to pacman
+		info.setNearestEdibleGhostToPacman(-1);
+		for (GHOST ghost : GHOST.values()) {
+			int ghostIndex = game.getGhostCurrentNodeIndex(ghost);
+			double ghostDistance = game.getDistance(ghostIndex,pacman,DM.PATH);
+			info.setDistanceGhostToPacman(ghost,ghostDistance);
+			if (game.isGhostEdible(ghost)) {
+				if (info.getNearestEdibleGhostToPacman() > ghostDistance) {
+					info.setNearestEdibleGhostToPacman(ghostIndex);
+				}
+			}
+		}
+
+		// if there is an edible ghost, set node of not edible ghost closest to them
+		int nearestEdibleGhost = info.getNearestEdibleGhostToPacman();
+		int closestGhost = -1;
+		if (nearestEdibleGhost != -1) {
+			double ghostDistance = Integer.MAX_VALUE;
+
+			for (GHOST ghost : GHOST.values()) {
+				if (!game.isGhostEdible(ghost)) {
+					int ghostIndex = game.getGhostCurrentNodeIndex(ghost);
+					double currDistance = game.getDistance(ghostIndex,nearestEdibleGhost,DM.PATH);
+
+					if (currDistance < ghostDistance) {
+						closestGhost = ghostIndex;
+						ghostDistance = currDistance;
+					}
+				}
+			}
+		}
+		this.info.setNearestGhost(closestGhost);
+
+
 	}
 
 	public boolean isBLINKYedible() {
@@ -60,6 +114,20 @@ public class GhostsInput extends Input {
 
 	public boolean isGhostInLair(GHOST ghost) {
 		return game.getGhostLairTime(ghost) > 0;
+	}
+
+	public int getNextJunctionNode(int node, MOVE move) {
+		int nextNode = this.game.getNeighbour(node,move);
+		int curr = node;
+		MOVE currMove = move;
+
+		while(!game.isJunction(nextNode)) {
+			curr = nextNode;
+			currMove = this.game.getPossibleMoves(curr,currMove)[0];
+			nextNode = this.game.getNeighbour(curr,currMove);
+		}
+
+		return nextNode;
 	}
 
 }
