0324f0d1a6ff56cdc32e38388271307b44c43266
juanpf04 <juanpf04@ucm.es>
2024-11-28 22:20:03 +0100

    fuzzytest y ghosts.fcl (solo faltan reglas)

--- Practica4/src/es/ucm/fdi/ici/c2425/practica4/grupo02/FuzzyTest.java
+++ Practica4/src/es/ucm/fdi/ici/c2425/practica4/grupo02/FuzzyTest.java
@@ -5,6 +5,7 @@ import java.io.File;
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.FunctionBlock;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 public class FuzzyTest {
 	
@@ -13,19 +14,37 @@ public class FuzzyTest {
 	private static final String RULES_FILE = "ghosts.fcl";
 
 	public static void main(String[] args) {
-		String rulesFile = String.format("%s%s", RULES_PATH, RULES_FILE);
 		
+		 // Load from 'FCL' file
+		String rulesFile = String.format("%s%s", RULES_PATH, RULES_FILE);
 		FIS fis = FIS.load(rulesFile, true);
 
-		if (fis == null) {
+		// Error while loading?
+		if (fis == null) { 
 			System.err.println("Can't load file: '" + rulesFile + "'");
 			System.exit(1);
 		}
 
-		// Get default function block
+		// Get function block
 		FunctionBlock fb = fis.getFunctionBlock("FuzzyGhosts");
 		
+		// Show
 		JFuzzyChart.get().chart(fb);
+
+		// Set inputs
+        fis.setVariable("MSPACMANdistance", 20);
+        fis.setVariable("MSPACMANconfidence", 0);
+        fis.setVariable("edible", 1);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable runAway = fb.getVariable("Runaway");
+        JFuzzyChart.get().chart(runAway, runAway.getDefuzzifier(), true);
+
+        Variable goToPPill = fb.getVariable("GoToPPill");
+        JFuzzyChart.get().chart(goToPPill, goToPPill.getDefuzzifier(), true);
 
 	}
 
