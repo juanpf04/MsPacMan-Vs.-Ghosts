f0a193a52b684437486a83814754089cb9afba37
juanpf04 <juanpf04@ucm.es>
2024-11-28 17:27:37 +0100

    test de archvios fcl

---
+++ Practica4/src/es/ucm/fdi/ici/c2425/practica4/grupo02/FuzzyTest.java
@@ -0,0 +1,32 @@
+package es.ucm.fdi.ici.c2425.practica4.grupo02;
+
+import java.io.File;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+
+public class FuzzyTest {
+	
+	private static final String RULES_PATH = "src"+File.separator+"es"+File.separator+"ucm"+File.separator+"fdi"+File.separator+"ici"+File.separator+"c2425"+File.separator+"practica4"+File.separator+"grupo02"+File.separator+"ghosts"+File.separator;
+
+	private static final String RULES_FILE = "ghosts.fcl";
+
+	public static void main(String[] args) {
+		String rulesFile = String.format("%s%s", RULES_PATH, RULES_FILE);
+		
+		FIS fis = FIS.load(rulesFile, true);
+
+		if (fis == null) {
+			System.err.println("Can't load file: '" + rulesFile + "'");
+			System.exit(1);
+		}
+
+		// Get default function block
+		FunctionBlock fb = fis.getFunctionBlock("FuzzyGhosts");
+		
+		JFuzzyChart.get().chart(fb);
+
+	}
+
+}
