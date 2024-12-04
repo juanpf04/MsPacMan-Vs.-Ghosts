3234fa8b5eb050267c6a55bf51ef2f696f4f8756
juanpf04 <juanpf04@ucm.es>
2024-12-04 14:47:08 +0100

    Update MsPacMan.java

--- Practica5/src/es/ucm/fdi/ici/c2425/practica5/grupo02/MsPacMan.java
+++ Practica5/src/es/ucm/fdi/ici/c2425/practica5/grupo02/MsPacMan.java
@@ -10,58 +10,53 @@ import pacman.game.Game;
 
 public class MsPacMan extends PacmanController {
 
-	MsPacManCBRengine cbrEngine;
-	MsPacManStorageManager storageManager;
-		
-	public MsPacMan()
-	{		
+	private MsPacManCBRengine cbrEngine;
+	private MsPacManStorageManager storageManager;
+
+	public MsPacMan() {
 		this.storageManager = new MsPacManStorageManager();
-		cbrEngine = new MsPacManCBRengine(storageManager);
+		this.cbrEngine = new MsPacManCBRengine(this.storageManager);
 	}
-	
+
 	@Override
 	public void preCompute(String opponent) {
-		cbrEngine.setOpponent(opponent);
+		this.cbrEngine.setOpponent(opponent);
 		try {
-			cbrEngine.configure();
-			cbrEngine.preCycle();
+			this.cbrEngine.configure();
+			this.cbrEngine.preCycle();
 		} catch (ExecutionException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	@Override
 	public void postCompute() {
 		try {
-			cbrEngine.postCycle();
+			this.cbrEngine.postCycle();
 		} catch (ExecutionException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	@Override
 	public MOVE getMove(Game game, long timeDue) {
-		
-		
-		//This implementation only computes a new action when MsPacMan is in a junction. 
-		//This is relevant for the case storage policy
-		if(!game.isJunction(game.getPacmanCurrentNodeIndex()))
-			return MOVE.NEUTRAL;
-		
-		
+		MOVE move = MOVE.NEUTRAL;
 		try {
-			MsPacManInput input = new MsPacManInput(game);
-			input.parseInput();
-			storageManager.setGame(game);
-			cbrEngine.cycle(input.getQuery());
-			MOVE move = cbrEngine.getSolution();
-			return move;
+			// This implementation only computes a new action when MsPacMan is in a
+			// junction.
+			// This is relevant for the case storage policy
+			if (game.isJunction(game.getPacmanCurrentNodeIndex())) {
+				MsPacManInput input = new MsPacManInput(game);
+				this.storageManager.setGame(game);
+				this.cbrEngine.cycle(input.getQuery());
+
+				move = this.cbrEngine.getSolution();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return MOVE.NEUTRAL;
+
+		return move;
 	}
 
 }
